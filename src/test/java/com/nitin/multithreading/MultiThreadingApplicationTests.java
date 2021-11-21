ad2c37196fbc1299b09ddc87efac86b8bce56207
ngaikwad3 <ngaikwad3@dxc.com>
2021-11-21 05:55:00 +0530

    First Commit

---
+++ src/test/java/com/nitin/multithreading/MultiThreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.multithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
